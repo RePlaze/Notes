0826e65fe7826a8c428bc21f6c9b25f465eeaf03
Ruslan Nazenov <nazenov13@outlook.com>
2023-05-07 22:15:57 +0700

    added  Display Notes | Recycler View #fix bugs

--- app/src/main/java/nazenov/notes/activities/MainActivity.java
+++ app/src/main/java/nazenov/notes/activities/MainActivity.java
@@ -7,14 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import nazenov.notes.R;
+import nazenov.notes.adapters.NotesAdapter;
+import nazenov.notes.database.NotesDataBase;
 import nazenov.notes.entities.Note;
 
 
 public class MainActivity extends Activity {
     public static final int REQUEST_CODE_ADD_NOTE = 1;
+    private RecyclerView notesRecyclerView;
+    private List<Note> noteList;
+    private NotesAdapter notesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,21 +39,39 @@ public class MainActivity extends Activity {
                 );
             }
         });
+
+        notesRecyclerView = findViewById(R.id.notesRecyclerView);
+        notesRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+        noteList = new ArrayList<>();
+        notesAdapter = new NotesAdapter(noteList);
+        notesRecyclerView.setAdapter(notesAdapter);
+
         getNotes();
     }
 
     private void getNotes() {
-        class GetNotesTask extends AsyncTask<Void,Void, List<Note>>{
-
+        class GetNotesTask extends AsyncTask<Void, Void, List<Note>> {
             @Override
             protected List<Note> doInBackground(Void... voids) {
-                return null;
+                return NotesDataBase
+                        .getDatabase(getApplicationContext())
+                        .noteDao().getAllNotes();
             }
-
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
+                if(noteList.size() == 0) {
+                    noteList.addAll(notes);
+                    notesAdapter.notifyDataSetChanged();
+                } else {
+                    noteList.add(0, notes.get(0));
+                    notesAdapter.notifyItemInserted(0);
+                }
+                notesRecyclerView.smoothScrollToPosition(0);
             }
         }
+        new GetNotesTask().execute();
     }
 }
